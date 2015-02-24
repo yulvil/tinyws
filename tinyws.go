8c65091c5ccb0f5cfece672f4ae01ba688e9044e
yulvil <p+github@pbcote.com>
2015-02-23 18:47:12 -0800

    Added ReverseProxy. Serve both http and https.

--- tinyws.go
+++ tinyws.go
@@ -1,49 +1,101 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
+	"sync"
 )
 
-type mapping struct {
-	Directory string
-	Path      string
+type Config struct {
+	HttpPort          int       `json:"http_port,omitempty"`
+	HttpsPort         int       `json:"https_port,omitempty"`
+	SslCertificate    string    `json:"ssl_certificate,omitempty"`
+	SslCertificateKey string    `json:"ssl_certificate_key,omitempty"`
+	Handlers          []Handler `json:"handlers,omitempty"`
 }
 
-func logRequest(r *http.Request) {
-	d, _ := httputil.DumpRequestOut(r, false)
-	fmt.Printf("%+v\n", d)
+type Handler struct {
+	Type        string `json:"type,omitempty"` // file, proxy
+	ContextPath string `json:"context_path,omitempty"`
+	Directory   string `json:"directory,omitempty"`
+	Backend     string `json:"backend,omitempty"`
 }
 
-var portNum int
-var directory string
-var path string
-var https bool
+func WebLog(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
 
-// var cfg string
+//func addCORS(handler http.Handler) http.Handler {
+//  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//      w.Header().Set("Access-Control-Allow-Origin", "*")
+//      w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With")
+//      handler.ServeHTTP(w, r)
+//  })
+//}
+
+var cfg string
 
 func init() {
-	flag.IntVar(&portNum, "port", 8080, "server port")
-	flag.StringVar(&directory, "dir", "./", "directory to serve")
-	flag.StringVar(&path, "path", "/", "context path")
-	// flag.StringVar(&cfg, "cfg", "", "json configuration")
-	flag.BoolVar(&https, "https", false, "run as https (cert.pem and key.pem required)")
+	flag.StringVar(&cfg, "cfg", "{}", "json configuration")
 	// openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days XXX -nodes
+}
+
+func loadConfig(cfg string) *Config {
+	config := new(Config)
+
+	if cfg[0] == '{' {
+		err := json.NewDecoder(bytes.NewBuffer([]byte(cfg))).Decode(config)
+		fmt.Printf("%+v\n", err)
+	} else {
+		buf, err := ioutil.ReadFile(cfg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = json.NewDecoder(bytes.NewBuffer(buf)).Decode(config)
+		fmt.Printf("%+v\n", err)
+	}
+	fmt.Printf("%+v\n", config)
+
+	return config
 }
 
 func main() {
 	flag.Parse()
 
-	fmt.Printf("Serving directory %s on port %d at path %s\n", directory, portNum, path)
-	fmt.Printf("http://localhost:%d%s\n", portNum, path)
+	config := loadConfig(cfg)
 
-	http.Handle(path, http.StripPrefix(path, http.FileServer(http.Dir(directory))))
-	if https {
-		log.Fatal(http.ListenAndServeTLS(fmt.Sprintf(":%d", portNum), "cert.pem", "key.pem", nil))
-	} else {
-		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", portNum), nil))
+	wg := &sync.WaitGroup{}
+
+	for _, handler := range config.Handlers {
+		if handler.Type == "file" {
+			http.Handle(handler.ContextPath, WebLog(http.StripPrefix(handler.ContextPath, http.FileServer(http.Dir(handler.Directory)))))
+		} else if handler.Type == "proxy" {
+			u, _ := url.Parse(handler.Backend)
+			proxy := httputil.NewSingleHostReverseProxy(u)
+			http.Handle(handler.ContextPath, WebLog(proxy))
+		}
 	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.HttpPort), nil))
+	}()
+
+	go func() {
+		defer wg.Done()
+		log.Fatal(http.ListenAndServeTLS(fmt.Sprintf(":%d", config.HttpsPort), "cert.pem", "key.pem", nil))
+	}()
+
+	wg.Wait()
 }
